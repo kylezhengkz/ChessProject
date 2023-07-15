5ce86de5ff6d31e5c64c79fafca334b17924928f
Kyle <kylezhengkz@gmail.com>
2023-07-14 21:49:20 -0400

    starting position update

--- Game.java
+++ Game.java
@@ -1,30 +1,35 @@
 import java.util.*;
-import java.util.Scanner;
+
 public class Game {
 
     public static final int WHITE = 1;
     public static final int BLACK = 2;
 
-    public static HashMap<int[], Piece> myPieces = new HashMap<>();
-    public static HashMap<int[], Piece> opponentPieces = new HashMap<>();
+    public static PositionNode currentPosition;
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         String input;
-        int color = - 1;
-        System.out.print("Enter 'W' to play as white, and 'B' to play as black:");
+        int color = -1;
+        System.out.print("Enter 'W' to play as white, and 'B' to play as black: ");
         input = sc.nextLine();
-        if (input == "W") {
+        if (input.equals("W")) {
             color = WHITE;
-        } else if (input == "B") {
+        } else if (input.equals("B")) {
             color = BLACK;
         } else {
             System.out.println("Invalid input. Terminating program.");
+            sc.close();
             System.exit(0);
         }
 
+        initStartingPosition(color);
+    }
+
+    private static void initStartingPosition(int color) {
+        HashMap<int[], Piece> myStartingPosition = new HashMap<>();
+        HashMap<int[], Piece> opponentStartingPosition = new HashMap<>();
 
         int[] square = new int[2];
 
@@ -36,7 +41,7 @@ public class Game {
         }
         for (int i = 1; i <= 8; i++) {
             square[0] = i;
-            myPieces.put(square, new Pawn(1, square, color));
+            myStartingPosition.put(square, new Pawn(1, square, color));
         }
 
         if (color == BLACK) {
@@ -46,107 +51,143 @@ public class Game {
         }
         for (int i = 1; i <= 8; i++) {
             square[0] = i;
-            opponentPieces.put(square, new Pawn(1, square, color));
+            opponentStartingPosition.put(square, new Pawn(1, square, color));
         }
 
         // insert rooks
         square[0] = 1;
         square[1] = 1;
         if (color == WHITE) {
-            myPieces.put(square, new Rook(5, square, WHITE));
+            myStartingPosition.put(square, new Rook(5, square, WHITE));
         } else {
-            opponentPieces.put(square, new Rook(5, square, WHITE));
+            opponentStartingPosition.put(square, new Rook(5, square, WHITE));
         }
 
         square[0] = 8;
         square[1] = 1;
         if (color == WHITE) {
-            myPieces.put(square, new Rook(5, square, WHITE));
+            myStartingPosition.put(square, new Rook(5, square, WHITE));
         } else {
-            opponentPieces.put(square, new Rook(5, square, WHITE));
+            opponentStartingPosition.put(square, new Rook(5, square, WHITE));
         }
 
         square[0] = 1;
         square[1] = 8;
         if (color == BLACK) {
-            myPieces.put(square, new Rook(5, square, BLACK));
+            myStartingPosition.put(square, new Rook(5, square, BLACK));
         } else {
-            opponentPieces.put(square, new Rook(5, square, BLACK));
+            opponentStartingPosition.put(square, new Rook(5, square, BLACK));
         }
 
         square[0] = 8;
         square[1] = 8;
         if (color == BLACK) {
-            myPieces.put(square, new Rook(5, square, BLACK));
+            myStartingPosition.put(square, new Rook(5, square, BLACK));
         } else {
-            opponentPieces.put(square, new Rook(5, square, BLACK));
+            opponentStartingPosition.put(square, new Rook(5, square, BLACK));
         }
 
         // insert knights
         square[0] = 2;
         square[1] = 1;
         if (color == WHITE) {
-            myPieces.put(square, new Knight(3, square, WHITE));
+            myStartingPosition.put(square, new Knight(3, square, WHITE));
         } else {
-            opponentPieces.put(square, new Knight(3, square, WHITE));
+            opponentStartingPosition.put(square, new Knight(3, square, WHITE));
         }
 
         square[0] = 7;
         square[1] = 1;
         if (color == WHITE) {
-            myPieces.put(square, new Knight(3, square, WHITE));
+            myStartingPosition.put(square, new Knight(3, square, WHITE));
         } else {
-            opponentPieces.put(square, new Knight(3, square, WHITE));
+            opponentStartingPosition.put(square, new Knight(3, square, WHITE));
         }
 
         square[0] = 2;
         square[1] = 8;
         if (color == BLACK) {
-            myPieces.put(square, new Knight(3, square, BLACK));
+            myStartingPosition.put(square, new Knight(3, square, BLACK));
         } else {
-            opponentPieces.put(square, new Knight(3, square, BLACK));
+            opponentStartingPosition.put(square, new Knight(3, square, BLACK));
         }
 
         square[0] = 7;
         square[1] = 8;
         if (color == BLACK) {
-            myPieces.put(square, new Knight(3, square, BLACK));
+            myStartingPosition.put(square, new Knight(3, square, BLACK));
         } else {
-            opponentPieces.put(square, new Knight(3, square, BLACK));
+            opponentStartingPosition.put(square, new Knight(3, square, BLACK));
         }
 
         // insert bishops
         square[0] = 3;
         square[1] = 1;
         if (color == WHITE) {
-            myPieces.put(square, new Bishop(3, square, WHITE));
+            myStartingPosition.put(square, new Bishop(3, square, WHITE));
         } else {
-            opponentPieces.put(square, new Bishop(3, square, WHITE));
+            opponentStartingPosition.put(square, new Bishop(3, square, WHITE));
         }
 
         square[0] = 6;
         square[1] = 1;
         if (color == WHITE) {
-            myPieces.put(square, new Bishop(3, square, WHITE));
+            myStartingPosition.put(square, new Bishop(3, square, WHITE));
         } else {
-            opponentPieces.put(square, new Bishop(3, square, WHITE));
+            opponentStartingPosition.put(square, new Bishop(3, square, WHITE));
         }
 
         square[0] = 3;
         square[1] = 8;
         if (color == BLACK) {
-            myPieces.put(square, new Bishop(3, square, BLACK));
+            myStartingPosition.put(square, new Bishop(3, square, BLACK));
         } else {
-            opponentPieces.put(square, new Bishop(3, square, BLACK));
+            opponentStartingPosition.put(square, new Bishop(3, square, BLACK));
         }
 
         square[0] = 6;
         square[1] = 8;
         if (color == BLACK) {
-            myPieces.put(square, new Bishop(3, square, BLACK));
+            myStartingPosition.put(square, new Bishop(3, square, BLACK));
         } else {
-            opponentPieces.put(square, new Bishop(3, square, BLACK));
+            opponentStartingPosition.put(square, new Bishop(3, square, BLACK));
         }
-        
+
+        // insert queens
+        square[0] = 4;
+        square[1] = 1;
+        if (color == WHITE) {
+            myStartingPosition.put(square, new Queen(9, square, WHITE));
+        } else {
+            opponentStartingPosition.put(square, new Queen(9, square, WHITE));
+        }
+
+        square[0] = 4;
+        square[1] = 8;
+        if (color == BLACK) {
+            myStartingPosition.put(square, new Queen(9, square, BLACK));
+        } else {
+            opponentStartingPosition.put(square, new Queen(9, square, BLACK));
+        }
+
+        // insert kings
+        square[0] = 5;
+        square[1] = 1;
+        if (color == WHITE) {
+            myStartingPosition.put(square, new King(13, square, WHITE));
+        } else {
+            opponentStartingPosition.put(square, new King(13, square, WHITE));
+        }
+
+        square[0] = 5;
+        square[1] = 8;
+        if (color == BLACK) {
+            myStartingPosition.put(square, new King(13, square, BLACK));
+        } else {
+            opponentStartingPosition.put(square, new King(13, square, BLACK));
+        }
+
+        currentPosition = new PositionNode(myStartingPosition, opponentStartingPosition);
     }
+
 }
